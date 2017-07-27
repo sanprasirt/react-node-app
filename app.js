cc71cc87938682b5db3425a9ca436ad43c006bf6
sanprasirt <kuforce@gmail.com>
2017-07-27 17:39:34 +0700

    update main jsx

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static(path.join(__dirname,"/html")));
+app.use(bodyParser.json());
 
 app.listen(7777, function(){
     console.log("Start listening on port", 7777);
+})
+
+app.post('/signin', function(req, res) {
+    var user_name = req.body.email;
+    var password = req.body.password;
+    if(user_name=='admin' && password=='admin'){
+        res.send('success');
+    }else{
+        res.send('Failure');
+    }
 })
